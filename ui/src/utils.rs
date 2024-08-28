bfc0d903769888d8f6a6aedfcf66fb6cbe293a7d
Abhinav Gunwant <abhigunwant@gmail.com>
2024-08-28 20:36:38 +0530

    feat: first start detection impl

--- ui/src/utils.rs
+++ ui/src/utils.rs
@@ -1,2 +1,19 @@
-pub fn is_first_start() -> bool { true }
+use std::path::Path;
+use dirs_next::data_local_dir;
+
+pub fn is_first_start() -> bool {
+    match data_local_dir() {
+        Some(mut path) => {
+            path.push("secure-notes");
+
+            if let Some(local_path) = path.to_str() {
+                return !Path::new(local_path).exists();
+            } else {
+                return true
+            }
+        }
+
+        None => false
+    }
+}
 
